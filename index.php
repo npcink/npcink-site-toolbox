33382bd9acd51f2360feac987642784eaf318c97
Muze-mac <13581261474@163.com>
2023-04-04 17:55:06 +0800

    更新接口

--- index.php
+++ index.php
@@ -1,52 +1,50 @@
 <?php //沉默是金
 
-//定义 REST API 端点（Endpoint）
-add_action('rest_api_init', 'mytheme_register_rest_endpoints');
-
-function mytheme_register_rest_endpoints()
-{
-    // Get theme options
-    register_rest_route('carbon-fields/v1', 'h5-options', array(
+//http://magick.plugin/wp-json/carbon-fields/v1/posts/2278
+add_action('rest_api_init', function () {
+    register_rest_route('carbon-fields/v1', 'posts/(?P<id>\d+)', array(
         'methods' => 'GET',
-        'callback' => 'mytheme_get_theme_options',
-        //权限控制
-        //'permission_callback' => function () {
-        //    return current_user_can('manage_options');
-        //},
+        'callback' => 'mytheme_get_post_info',
+        'permission_callback' => '__return_true',
     ));
+});
 
-}
-
-//返回选项值
-function mytheme_get_theme_options($request)
+function mytheme_get_post_info($request)
 {
-    $fields = [
-        'comm_h5_index_tone',
-        'comm_h5_index_tone_cat',
-        'comm_h5_index_category',
-        'comm_h5_single_contact_title',
-        'comm_h5_single_contact_one_title',
-        'comm_h5_single_contact_one_content',
-        'comm_h5_single_contact_two_title',
-        'comm_h5_single_contact_two_content',
-        'comm_h5_singel_featured_link',
-        'comm_h5_singel_featured_logo',
-        'comm_h5_singel_featured_msg',
-    ];
+    $post_id = $request->get_param('id');
+    $post = get_post($post_id);
 
-    $options = [];
-    foreach ($fields as $field) {
-        $options[$field] = carbon_get_theme_option($field);
-        //处理数组
-        if ($field == "comm_h5_index_tone") {
-            //创建数组存储数据
-            $arr = [];
-            $sum = carbon_get_theme_option($field);
-            $arr = array_map(function ($obj) {return $obj['id'];}, $sum);
-            $options[$field] = $arr;
-        }
+    $post_title = get_the_title($post_id);
+    $post_excerpt = $post->post_excerpt; // 获取文章描述
+    $post_date = get_the_date('Y-m-d H:i:s', $post_id);
 
+    $post_categories = get_the_category($post_id);
+    $cat_array = array();
+    foreach ($post_categories as $cat) {
+        $cat_array[] = array(
+            'id' => $cat->cat_ID,
+            'name' => $cat->name,
+        );
     }
-    return $options;
+    $categories = $cat_array;
 
+    $featured_image = array();
+    if (has_post_thumbnail($post_id)) {
+        $thumbnail_id = get_post_thumbnail_id($post_id);
+        $featured_image['url'] = wp_get_attachment_url($thumbnail_id);
+        $featured_image['caption'] = get_post($thumbnail_id)->post_excerpt;
+        $featured_image['details'] = wp_get_attachment_metadata($thumbnail_id);
+    }
+    $post_content = apply_filters('the_content', $post->post_content); // 获取文章正文内容
+    $response = array(
+        'id' => $post_id,
+        'date' => $post_date,
+        'title' => $post_title,
+        'excerpt' => $post_excerpt,
+        'image' => $featured_image,
+        'category' => $categories,
+        'content' => $post_content,
+    );
+
+    return $response;
 }
--- magick-mixtrue.php
+++ magick-mixtrue.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: 魔法合剂插件
 Plugin URI: https://www.npc.ink/
-Description: 目前主要是统计功能
-Version: 0.0.3
+Description: 文章统计+小功能
+Version: 0.1.1
 Author: Muze
 Author URI: https://www.npc.ink/
  */
@@ -56,3 +56,55 @@ run_magick_mixture();
  * 为WordPress后台的文章、分类等显示ID
  */
 // 添加一个新的列 ID
+
+// 定义 REST API 端点（Endpoint）
+add_action('rest_api_init', 'mytheme_register_rest_endpoints');
+
+function mytheme_register_rest_endpoints()
+{
+    // Get theme options
+    register_rest_route('carbon-fields/v1', 'h5-options', array(
+        'methods' => 'GET',
+        'callback' => 'mytheme_get_theme_options',
+        // 权限控制
+        // 'permission_callback' => function () {
+        //     return current_user_can('manage_options');
+        // },
+    ));
+}
+
+//返回选项值
+function mytheme_get_theme_options($request)
+{
+    $fields = [
+        'comm_h5_index_tone',
+        'comm_h5_index_tone_cat',
+        'comm_h5_index_category',
+        'comm_h5_single_contact_title',
+        'comm_h5_single_contact_one_title',
+        'comm_h5_single_contact_one_content',
+        'comm_h5_single_contact_two_title',
+        'comm_h5_single_contact_two_content',
+        'comm_h5_singel_featured_link',
+        'comm_h5_singel_featured_logo',
+        'comm_h5_singel_featured_msg',
+    ];
+
+    $options = [];
+    foreach ($fields as $field) {
+        $options[$field] = carbon_get_theme_option($field);
+        //处理数组
+        if ($field == "comm_h5_index_tone") {
+            //创建数组存储数据
+            $arr = [];
+            $sum = carbon_get_theme_option($field);
+            $arr = array_map(function ($obj) {return $obj['id'];}, $sum);
+            //转成数组
+            $intArray = array_map('intval', $arr);
+            $options[$field] = $intArray;
+        }
+
+    }
+    return $options;
+
+}
