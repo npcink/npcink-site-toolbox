c7e828211e3ca0861ec0322d7730017c1ed1501d
Muze-mac <13581261474@163.com>
2023-08-04 09:54:47 +0800

    优化下

--- vite/admin/src/App.tsx
+++ vite/admin/src/App.tsx
@@ -1,16 +1,16 @@
-
+import React from "react";
+import { Button, Space } from "antd";
 
 import Tab from "./components/tab";
 
-function App() {
- 
-
-  return (
+const App: React.FC = () => (
     <>
-     
       <Tab />
+      <Space wrap>
+        <Button type="primary" >保存</Button>
+      </Space>
     </>
   );
-}
+
 
 export default App;
