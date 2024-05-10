2028e68ad1b9a6eaa2d5d441f309bd626a2e5be5
Muze-mac <13581261474@163.com>
2024-05-10 18:28:25 +0800

    优化结构

--- admin/partials/page/exterior/index.php
+++ admin/partials/page/exterior/index.php
@@ -15,14 +15,14 @@ if (!class_exists('Npcink_Page_Exterior')) {
             $particle = MaMi_Admin::get_config($option, 'particle', "false");
             //四散
             if ($particle === "diffuse") {
-                require_once plugin_dir_path(__FILE__) . 'page_add_particle.php';
+                require_once plugin_dir_path(__FILE__) . 'add_particle.php';
                 Npcink_Page_Add_Particle::run();
             }
 
             //美化滚动条
             $scrol = MaMi_Admin::get_config($option, 'scrol');
             if ($scrol !== "false") {
-                require_once plugin_dir_path(__FILE__) . 'page_scroll_bar.php';
+                require_once plugin_dir_path(__FILE__) . 'add_scroll_bar.php';
                 Npcink_Page_Add_Scroll_Bar::run($scrol);
             }
         }
