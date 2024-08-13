571c5ad8cedebd41ec224de65a940b36465dfa32
Muze-mac <13581261474@163.com>
2024-08-13 16:12:21 +0800

    优化删除插件时删除数据的方法

--- uninstall.php
+++ uninstall.php
@@ -25,49 +25,18 @@
  * @package    Dema
  */
 
-// If uninstall not called from WordPress, then exit.
+// 如果未从WordPress调用卸载，请退出。
 if (!defined('WP_UNINSTALL_PLUGIN')) {
 	exit;
 }
 
 
-
-//执行卸载插件时的动作
-//require plugin_dir_path(__FILE__) . 'admin/partials/function/config/remove_config.php';
-//function run_mare_uninstall()
-//{
-//	$plugin = new MaBox_Config_Remove_Config();
-//	$plugin->run();
-//}
-//run_mare_uninstall();
-
-//TODO:待检查
-function delete_custom_field_from_all_posts($field_key)
-{
-	$args = array(
-		'post_type' => array('post', 'page'),  // 可以修改为其他自定义文章类型
-		'posts_per_page' => -1,  // 获取所有文章
-		'post_status' => 'any',  // 获取所有状态的文章
-	);
-
-	$posts = get_posts($args);
-
-	foreach ($posts as $post) {
-		$post_id = $post->ID;
-		$existing_value = get_post_meta($post_id, $field_key, true);
-
-		if (!empty($existing_value)) {
-			delete_post_meta($post_id, $field_key);
-			echo "Deleted custom field '{$field_key}' from post {$post_id}.<br>";
-		} else {
-			echo "Custom field '{$field_key}' not found in post {$post_id}.<br>";
-		}
-	}
-}
-
-// 使用示例：删除名为 'custom_field_options' 的自定义字段
-delete_custom_field_from_all_posts('mabox_trends_special');
+/**
+ * mabox_trends_special：专题页面 - 搜索词，
+ */
+//删除自定义字段
+delete_post_meta_by_key('mabox_trends_special');
 
 
-//delete_option("Magick_ToolBox_Option");
-delete_option(MAGICK_MIXTURE_OPTION);
+//删除选项数据
+delete_option("Magick_ToolBox_Option");
